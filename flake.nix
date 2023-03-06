1804c8a524937197894501cd8c111aa2973a662b
Takuma Suzuki <pferd262@gmail.com>
2023-03-06 17:01:22 +0900

    :saprkles: Add Makefile
    とりあえず docker build コマンドを入れておく

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "A very basic flake";
 
   outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-      nodejs = pkgs.nodejs-16_x;
-    in
-    with pkgs; {
+{
+      devShell.x86_64-linux = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in with pkgs; mkShell {
+        buildInputs = [ nodejs-16_x yarn kind gnumake ];
+      };
 
-      devShell.x86_64-darwin = mkShell { buildInputs = [ nodejs yarn ]; };
-
+      devShell.x86_64-darwin = let pkgs = nixpkgs.legacyPackages.x86_64-darwin; in with pkgs; mkShell {
+        buildInputs = [ nodejs-16_x yarn kind gnumake ];
+      };
     };
 }
